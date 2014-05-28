6fef656f6275ed493190c275646808a85c85ace2
starforever <starforever00@gmail.com>
2014-05-29 00:17:12 +0800

    Successfully parsed card list from detailed deck page

--- collector_hearthpwn.py
+++ collector_hearthpwn.py
@@ -1,4 +1,5 @@
 import datetime
+import re
 import lxml.html
 from model import Deck
 from database_op import database_connect
@@ -6,11 +7,26 @@ from database_op import database_connect
 # REST_INTERVAL = 3
 DOMAIN = 'http://www.hearthpwn.com'
 
+DUST_COST_MATCHER = re.compile('.*Crafting Cost: (\d+).*', re.DOTALL)
+CARD_COUNT_MATCHER = re.compile(u'.*\xd7 (\d+).*', re.DOTALL)
+
+def parse_deck (deck):
+  url = DOMAIN + deck.url
+  info = lxml.html.parse(url).getroot().find_class('infobox')[0]
+  deck.dust_cost = int(DUST_COST_MATCHER.match(info.find_class('t-deck-dust-cost')[0].text_content()).groups()[0])
+  rows = []
+  for sec in info.find_class('t-deck-details-card-list'):
+    rows.extend(sec.find_class('listing')[0].xpath('tbody/tr'))
+  deck.cards = []
+  for row in rows:
+    num = int(CARD_COUNT_MATCHER.match(row.find_class('col-name')[0].text_content()).groups()[0])
+    deck.cards.extend([row.find_class('col-name')[0].xpath('b/a')[0].text_content()] * num)
+
 def parse_page (pagenum):
   url = DOMAIN + '/decks?filter-is-forge=2&sort=-datemodified&page=%d' % pagenum
   root = lxml.html.parse(url).getroot()
-  decks = root.get_element_by_id('decks').xpath('tbody/tr')
-  for row in decks:
+  rows = root.get_element_by_id('decks').xpath('tbody/tr')
+  for row in rows:
     deck = Deck()
     deck.url = row.find_class('col-name')[0].xpath('div/span/a')[0].attrib['href']
     deck.name = row.find_class('col-name')[0].xpath('div/span/a')[0].text_content()
@@ -20,9 +36,10 @@ def parse_page (pagenum):
     deck.rating = int(row.find_class('col-ratings')[0].xpath('div')[0].text_content())
     deck.num_view = int(row.find_class('col-views')[0].text_content())
     deck.num_comment = int(row.find_class('col-comments')[0].text_content())
-    deck.dust_cost = 0
     deck.time_update = datetime.datetime.fromtimestamp(int(row.find_class('col-updated')[0].xpath('abbr')[0].attrib['data-epoch']))
+    parse_deck(deck)
     print deck
+    break
 
 if __name__ == '__main__':
   # database_name = parse_arg((str), 1)
--- model.py
+++ model.py
@@ -1,3 +1,3 @@
 class Deck ():
   def __str__ (self):
-    return '%s (by %s)\nType: %s\nClass: %s\nRating: %d\nViews: %d\nComments: %d\nCost: %d\nUpdated: %s\n' % (self.name.encode('utf-8'), self.author.encode('utf-8'), self.type, self.hclass, self.rating, self.num_view, self.num_comment, self.dust_cost, self.time_update)
+    return '%s (by %s)\nURL: %s\nType: %s\nClass: %s\nRating: %d\nViews: %d\nComments: %d\nCost: %d\nUpdated: %s\nCards:\n%s\n' % (self.name.encode('utf-8'), self.author.encode('utf-8'), self.url, self.type, self.hclass, self.rating, self.num_view, self.num_comment, self.dust_cost, self.time_update, '\n'.join(self.cards))
