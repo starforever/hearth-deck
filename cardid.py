f94b49a4a785938d52ce7e9b9aa7d6f287d1ae4a
starforever <starforever00@gmail.com>
2014-05-29 22:30:44 +0800

    Reverse mapping from card ID to name

--- cardid.py
+++ cardid.py
@@ -1,7 +1,9 @@
 def load (filename):
   global CARD_ID
+  global CARD_NAME
   print 'Loading card id mapping from %s' % filename
   CARD_ID = {}
+  CARD_NAME = []
   cid = 0
   fin = open(filename, 'r')
   while True:
@@ -9,14 +11,19 @@ def load (filename):
     if not line:
       break
     line = line[:-1]
-    cid += 1
-    for name in line.split(';'):
+    names = line.split(';')
+    CARD_NAME.append(names[0])
+    for name in names:
       if name in CARD_ID:
         raise Exception('Duplicate card names: %s' % name)
       CARD_ID[name] = cid
+    cid += 1
   fin.close()
 
-def get_id (name):
+def get_card_id (name):
   return CARD_ID[name]
+
+def get_card_name (id):
+  return CARD_NAME[id]
 
 load('cardid.txt')
