877009ddf9c3793468778006941cb02edff5c9e2
starforever <starforever00@gmail.com>
2015-05-11 13:32:23 -0700

    * Improved disk IO through batched database comitting * Updated card database with uncollectible cards from Blackrock Mountian expansion

--- collector_hearthpwn.py
+++ collector_hearthpwn.py
@@ -3,7 +3,7 @@ import re
 import lxml.html
 from util import parse_arg
 from deck import Deck
-from database_op import database_connect, database_close, deck_create, deck_insert, deck_find_by_id, deck_remove_unscanned
+from database_op import database_connect, database_close, database_commit, database_rollback, deck_create, deck_insert, deck_find_by_id, deck_remove_unscanned
 from card_info import card_by_name
 from key_store import save as save_key, load as load_key
 
@@ -13,6 +13,8 @@ DOMAIN = 'http://www.hearthpwn.com'
 DUST_COST_MATCHER = re.compile('.*Crafting Cost: -?(\d+).*', re.DOTALL)
 DECK_ID_MATCHER = re.compile('/decks/(\d+).*')
 CARD_COUNT_MATCHER = re.compile(u'.*\xd7 -?(\d+).*', re.DOTALL)
+
+DECK_BUFFER_SIZE = 64;
 
 def get_page_root (url):
   root = None
@@ -24,6 +26,21 @@ def get_page_root (url):
       print 'Retry after %d seconds.' % REST_INTERVAL
       time.sleep(REST_INTERVAL)
   return root
+
+def parse_row (row):
+  deck = Deck()
+  deck.url = row.find_class('col-name')[0].xpath('div/span/a')[0].attrib['href']
+  deck.id = int(DECK_ID_MATCHER.match(deck.url).groups()[0])
+  deck.name = row.find_class('col-name')[0].xpath('div/span/a')[0].text_content()
+  deck.author = row.find_class('col-name')[0].xpath('div/small/a')[0].text_content()
+  deck.type = row.find_class('col-deck-type')[0].text_content()
+  deck.hero_class = row.find_class('col-class')[0].text_content()
+  deck.rating = int(row.find_class('col-ratings')[0].xpath('div')[0].text_content())
+  deck.num_view = int(row.find_class('col-views')[0].text_content())
+  deck.num_comment = int(row.find_class('col-comments')[0].text_content())
+  deck.time_update = time.localtime(int(row.find_class('col-updated')[0].xpath('abbr')[0].attrib['data-epoch']))
+  deck.scan_count = SCAN_COUNT
+  return deck
 
 def parse_deck (deck):
   url = DOMAIN + deck.url
@@ -45,21 +62,6 @@ def parse_deck (deck):
         print 'Retry after %d seconds.' % REST_INTERVAL
         time.sleep(REST_INTERVAL)
 
-def parse_row (row):
-  deck = Deck()
-  deck.url = row.find_class('col-name')[0].xpath('div/span/a')[0].attrib['href']
-  deck.id = int(DECK_ID_MATCHER.match(deck.url).groups()[0])
-  deck.name = row.find_class('col-name')[0].xpath('div/span/a')[0].text_content()
-  deck.author = row.find_class('col-name')[0].xpath('div/small/a')[0].text_content()
-  deck.type = row.find_class('col-deck-type')[0].text_content()
-  deck.hero_class = row.find_class('col-class')[0].text_content()
-  deck.rating = int(row.find_class('col-ratings')[0].xpath('div')[0].text_content())
-  deck.num_view = int(row.find_class('col-views')[0].text_content())
-  deck.num_comment = int(row.find_class('col-comments')[0].text_content())
-  deck.time_update = time.localtime(int(row.find_class('col-updated')[0].xpath('abbr')[0].attrib['data-epoch']))
-  deck.scan_count = SCAN_COUNT
-  return deck
-
 def process_deck (deck):
   old_deck = Deck.from_database(deck_find_by_id(deck.id))
   if old_deck is not None and deck.time_update == old_deck.time_update:
@@ -71,44 +73,53 @@ def process_deck (deck):
     status = 'Update' if old_deck is not None else 'New'
   if not deck.is_valid():
     status += ', Invalid'
-  deck_insert(deck)
   return status
 
-def parse_page (pagenum):
+def parse_page (pagenum, rownum):
+  global DECK_BUFFER_NUM
   print '(%s) Parsing page %d...' % (time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()), pagenum)
   url = DOMAIN + '/decks?filter-is-forge=2&sort=-datemodified&page=%d' % pagenum
+  root = get_page_root(url)
+  rows = root.get_element_by_id('decks').xpath('tbody/tr')
+  while rownum < len(rows):
+    deck = parse_row(rows[rownum])
+    status = process_deck(deck)
+    deck_insert(deck)
+    DECK_BUFFER_NUM += 1
+    rownum += 1
+    print '  [%d] (%s) %s' % (deck.id, status, deck.name)
+    if DECK_BUFFER_NUM >= DECK_BUFFER_SIZE:
+      database_commit()
+      DECK_BUFFER_NUM = 0
+      save_key('CURRENT_PAGE', pagenum)
+      save_key('CURRENT_ROW', rownum)
+  has_next = 'Next' in [e.text_content() for e in root.find_class('paging-list')[0].xpath('li/a')]
+  return has_next
+
+def parse ():
+  global DECK_BUFFER_NUM, SCAN_COUNT
   while True:
     try:
-      root = get_page_root(url)
-      rows = root.get_element_by_id('decks').xpath('tbody/tr')
+      DECK_BUFFER_NUM = 0
+      SCAN_COUNT = load_key('SCAN_COUNT', 1)
+      pagenum = load_key('CURRENT_PAGE', 1)
       rownum = load_key('CURRENT_ROW', 0)
-      while rownum < len(rows):
-        deck = parse_row(rows[rownum])
-        status = process_deck(deck)
-        rownum += 1
-        save_key('CURRENT_ROW', rownum)
-        print '  [%d] (%s) %s' % (deck.id, status, deck.name)
-      has_next = 'Next' in [e.text_content() for e in root.find_class('paging-list')[0].xpath('li/a')]
-      return has_next
+      while parse_page(pagenum, rownum):
+        pagenum += 1
+        rownum = 0
+      deck_remove_unscanned(SCAN_COUNT)
+      break
     except (IndexError, KeyError) as e:
+      database_rollback()
       print e
       print 'Retry after %d seconds.' % REST_INTERVAL
       time.sleep(REST_INTERVAL)
-
-def parse ():
-  global SCAN_COUNT
-  SCAN_COUNT = load_key('SCAN_COUNT', 1)
-  pagenum = load_key('CURRENT_PAGE', 1)
-  while parse_page(pagenum):
-    pagenum += 1
-    save_key('CURRENT_PAGE', pagenum)
-    save_key('CURRENT_ROW', 0)
-  deck_remove_unscanned(SCAN_COUNT)
-  print 'Scan pass %d finished!' % SCAN_COUNT
+  database_commit()
   SCAN_COUNT += 1
   save_key('SCAN_COUNT', SCAN_COUNT)
   save_key('CURRENT_PAGE', 1)
   save_key('CURRENT_ROW', 0)
+  print 'Scan pass %d finished!' % (SCAN_COUNT - 1)
 
 if __name__ == '__main__':
   (database_name,) = parse_arg((str,), 1)
--- database_op.py
+++ database_op.py
@@ -11,6 +11,12 @@ def database_connect (filename):
 def database_close ():
   CONN.close()
 
+def database_commit ():
+  CONN.commit()
+
+def database_rollback ():
+  CONN.rollback()
+
 def deck_create ():
   CONN.execute("""CREATE TABLE IF NOT EXISTS decks (
     id INTEGER PRIMARY KEY,
@@ -27,11 +33,9 @@ def deck_create ():
     cards BLOB,
     scan_count INTEGER
   )""")
-  CONN.commit()
 
 def deck_insert (deck):
   CONN.execute("""INSERT OR REPLACE INTO decks VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""", (deck.id, deck.name, deck.author, deck.url, deck.type, deck.hero_class, deck.dust_cost, deck.rating, deck.num_view, deck.num_comment, int(time.mktime(deck.time_update)), buffer(pickle_dumps(deck.cards, -1)), deck.scan_count))
-  CONN.commit()
 
 def deck_find_by_id (id):
   return CONN.execute("""SELECT * FROM decks WHERE id = ?""", (id,)).fetchone()
@@ -41,4 +45,3 @@ def deck_select_by_class (hero_class):
 
 def deck_remove_unscanned (scan_count):
   CONN.execute("""DELETE FROM decks WHERE scan_count < ?""", (scan_count,))
-  CONN.commit()
